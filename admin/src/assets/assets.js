5d35b6951920a0ef1d36d0ec07a588db46e86643
vedvart <apple@MacBook-Pro.local>
2025-08-15 17:52:51 +0530

    authservice

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,8 +1,10 @@
 import dish from './dish_22.webp'
 import upload from './upload.png'
 import user from './user.png'
+import logo from './foody_logo2.0.jpg'
 export const assets ={
     dish,
     upload,
-    user
+    user,
+    logo
 }
